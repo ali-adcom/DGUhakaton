cf4257e30abff4b881e6c99966e421f4f24d9414
Ali <78687869+ali-adcom@users.noreply.github.com>
2024-11-30 22:59:39 +0300

    Update models.py

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Family(models.Model):
     title = models.CharField(max_length=64, verbose_name='Название')
     avatar = models.ImageField(upload_to='family_avatars/', null=True, blank=True, verbose_name='Аватар')
-    admin = models.ForeignKey('User', on_delete=models.CASCADE, related_name='admin_families', verbose_name='Администратор')
+    admin = models.OneToOneField('User', on_delete=models.CASCADE, related_name='family', verbose_name='Администратор')
 
     def __str__(self):
         return self.title
@@ -34,13 +34,13 @@ class User(AbstractBaseUser):
     family = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True, blank=True, related_name='members', verbose_name='Семья')
     gender = models.CharField(max_length=2, choices=GENDER_CHOICES, verbose_name='Пол')
     age = models.SmallIntegerField(verbose_name='Возвраст')
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
     
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-        ordering = ['last_name', 'first_name']
 
 
 class FamilyInviteCode(models.Model):
