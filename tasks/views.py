7faeb40114be93abbbf7d35755a318db2358ad0b
Ali <78687869+ali-adcom@users.noreply.github.com>
2024-12-01 13:20:55 +0300

    review

--- tasks/views.py
+++ tasks/views.py
@@ -10,22 +10,22 @@ from .serializers import TaskSerializer
 
 
 class TaskViewSet(GenericViewSet, CreateModelMixin, DestroyModelMixin, UpdateModelMixin, RetrieveModelMixin, ListModelMixin):
-    model = Task
+    queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
-    def get_queryset(self):
-        queryset = Task.objects.all()
-        return queryset
     
     
     @action(detail=True, methods=['POST'], url_path='complete', serializer_class=TaskSerializer)
     def complete_task(self, request, *args, **kwargs):
         task = self.get_object()
-        if request.user.family != task.family.admin: 
-            return Response({'error': 'Ты не глава семьи'}, status=status.HTTP_403_FORBIDDEN) 
+        
+        if request.user.family != task.family: 
+            return Response({'error': 'Вы не можете закрывать задачи другой семьи'}, status=status.HTTP_403_FORBIDDEN) 
+            
         task.is_completed = True
         task.closed_datetime = datetime.now() 
         task.closed_by = request.user 
         task.save()
+        
         serializer = self.get_serializer(task) 
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(serializer.data)
