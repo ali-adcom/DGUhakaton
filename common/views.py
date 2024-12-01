198a63e16166337fec8250c261ec50d3461aa46e
Ali <78687869+ali-adcom@users.noreply.github.com>
2024-12-01 13:16:37 +0300

    Update views.py

--- common/views.py
+++ common/views.py
@@ -9,11 +9,15 @@ from tasks.serializers import TaskSerializer
 @api_view(['GET']) 
 def get_main_page(request): 
     user = request.user 
-    today = datetime.today().date()
-    daily_tasks = Task.objects.filter(family=user.family, created_datetime__date=today)
-    tasks_serializer = TaskSerializer(daily_tasks, many=True)
-    completed_tasks = Task.objects.filter(closed_by=user).count()
-    user_score = { 'username': user.first_name + ' ' + user.last_name, 
-                  'completed_tasks': completed_tasks,
-                  }
-    return Response({'daily_tasks': tasks_serializer.data, 'user_score': user_score }, status=status.HTTP_200_OK)
+    
+    if user.is_authenticated:
+        today = datetime.today().date()
+        daily_rating_tasks = Task.objects.filter(family=user.family, created_datetime__date=today, scope='rating')
+        tasks_serializer = TaskSerializer(daily_rating_tasks, many=True)
+
+    data = {
+        'username': f'{user.first_name} {user.first_name}', 
+        'daily_tasks': tasks_serializer.data,
+    }
+    
+    return Response(data)
