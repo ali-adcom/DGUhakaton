0f2869e61499b162fb9b1722105907750cd8b546
Ali <78687869+ali-adcom@users.noreply.github.com>
2024-12-01 13:22:50 +0300

    Update views.py

--- common/views.py
+++ common/views.py
@@ -9,15 +9,19 @@ from tasks.serializers import TaskSerializer
 @api_view(['GET']) 
 def get_main_page(request): 
     user = request.user 
+
+    data = dict()
     
     if user.is_authenticated:
         today = datetime.today().date()
         daily_rating_tasks = Task.objects.filter(family=user.family, created_datetime__date=today, scope='rating')
         tasks_serializer = TaskSerializer(daily_rating_tasks, many=True)
 
-    data = {
-        'username': f'{user.first_name} {user.first_name}', 
-        'daily_tasks': tasks_serializer.data,
-    }
-    
+        data.update(
+            {
+                'username': f'{user.first_name} {user.first_name}', 
+                'daily_tasks': tasks_serializer.data,
+            }
+        )
+
     return Response(data)
